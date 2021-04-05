c1b0fe0cc6c00e14a6ff764ded19747ad0523c62
YousefBe <62329931+YousefBe@users.noreply.github.com>
2021-04-05 12:06:07 +0200

    laravel auth app with jwt and  vue js for frontend

---
+++ LaravelVuejwtAuth/frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <the-navigation></the-navigation>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import TheNavigation from "./components/TheNavigation.vue";
+
+export default {
+  components: {
+    TheNavigation,
+  },
+};
+</script>
---
+++ LaravelVuejwtAuth/frontend/src/views/dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    dashboard
+  </div>
+</template>
+
+<script>
+export default {
+  name: "dashboard",
+};
+</script>
+
+<style></style>
